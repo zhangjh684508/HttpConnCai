c7d53d975215c74dc41eb3a322dbf753435b1316
zhangjh684508 <583887669@qq.com>
2016-07-29 10:57:57 +0800

    conn to server

--- app/src/main/java/com/example/lsx/welcome/MainActivity.java
+++ app/src/main/java/com/example/lsx/welcome/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 HttpUtils http = new HttpUtils();
                 http.send(HttpRequest.HttpMethod.GET,
-                        "http://tapi.test.tuoguibao.com/basicapi.php?ac=1002&mobile=18562172800&password=123456",
+                        "http://192.3.19.42:8080?ac=1002&mobile=99999999&password=123456",
                         new RequestCallBack<String>(){
                             @Override
                             public void onLoading(long total, long current, boolean isUploading) {
